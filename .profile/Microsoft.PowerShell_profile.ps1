3567365d48d74b8c6cad754364fa3b7a9c7e9e3d
barts <b.steensma@fleetcleaner.com>
2022-02-05 12:42:28 +0100

    new folder structure

--- .profile/Microsoft.PowerShell_profile.ps1
+++
@@ -1,6 +0,0 @@
-# Profile for the Microsoft.Powershell Shell, only. (Not Visual Studio or other PoSh instances)
-# ===========
-
-Push-Location (Split-Path -parent $profile)
-"components-shell" | Where-Object {Test-Path "$_.ps1"} | ForEach-Object -process {Invoke-Expression ". .\$_.ps1"}
-Pop-Location
