56f62dff07ad30d7cbdaec55aa5aa75634216d0d
barts <b.steensma@fleetcleaner.com>
2022-02-08 08:46:19 +0100

    Refactored powershell directory

---
+++ dotfiles/powershell/main.ps1
@@ -0,0 +1,7 @@
+$profileDir = Split-Path -parent $profile
+New-Item $profileDir -ItemType Directory -Force -ErrorAction SilentlyContinue
+
+Get-ChildItem -Path $PSScriptRoot -Exclude main.ps1 -Name|
+Foreach-Object {
+    New-Item -ItemType SymbolicLink -Path $profileDir\$_ -Target $PSScriptRoot\$_ -Force
+}
