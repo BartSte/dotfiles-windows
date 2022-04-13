bd9a2860c2eae7c7d282cd4ac92c2528352f67a7
barts <b.steensma@fleetcleaner.com>
2022-04-13 10:39:11 +0200

    added optional neovim initialization

---
+++ dotfiles-windows/vim/main.ps1
@@ -0,0 +1,23 @@
+function symbolic-link-configuration {
+    $target = "${HOME}\.vimrc"
+    $path = "${env:LOCALAPPDATA}\nvim\init.vim" 
+
+    write-host $path
+    Remove-Item $path -ErrorAction SilentlyContinue
+    New-Item -ItemType SymbolicLink -Path $path -Target $target
+}
+
+function install_vim_plug() {
+    iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
+        ni $HOME/vimfiles/autoload/plug.vim -Force
+}
+
+function install_vim_plug_nvim() {
+    iwr -useb https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim |`
+        ni "$(@($env:XDG_DATA_HOME, $env:LOCALAPPDATA)[$null -eq $env:XDG_DATA_HOME])/nvim-data/site/autoload/plug.vim" -Force
+}
+
+Admin-Check
+# symbolic-link-configuration 
+install_vim_plug
+# install_vim_plug_nvim
