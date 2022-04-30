c46905952bee47e0f2405029cb980b2f5b0967ea
barts <b.steensma@fleetcleaner.com>
2022-04-30 18:16:36 +0200

    improved fzf

--- dotfiles-windows/powershell/fzf.ps1
+++ dotfiles-windows/powershell/fzf.ps1
@@ -1,27 +1,20 @@
-$commandOverride = [ScriptBlock]{ param($Location) cd $Location }
+$Env:EDITOR="gvim.bat"
+$Env:_PSFZF_FZF_DEFAULT_OPTS="--height 40% --reverse"
+$Env:FZF_DEFAULT_OPTS = '--bind ctrl-a:select-all,ctrl-d:deselect-all'
+$Env:FZF_DEFAULT_COMMAND = 'ag --hidden --ignore .git --ignore AppData --ignore "Reporting Data" --ignore "Fleet Cleaner" -g .'
 
-function Fuzzy-File-Get() {
-    $file = Get-ChildItem . -Recurse -File | Invoke-Fzf
-    [Microsoft.PowerShell.PSConsoleReadLine]::Insert($file)
-}
 
 function Fuzzy-Directory-Get() {
     $file = Get-ChildItem . -Recurse -Attributes Directory | Invoke-Fzf 
     [Microsoft.PowerShell.PSConsoleReadLine]::Insert($file)
 }
 
-
-Set-PsFzfOption -PSReadlineChordReverseHistory 'Ctrl+r' `
-                -AltCCommand $commandOverride
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' `
+                -PSReadlineChordReverseHistory 'Ctrl+r' `
 
 Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
 Set-PSReadLineKeyHandler -Key 'alt+o' -ScriptBlock { Invoke-FuzzyEdit }
-Set-PSReadLineKeyHandler -Key 'alt+d' -ScriptBlock { Fuzzy-Directory-Get }
-Set-PSReadLineKeyHandler -Key 'ctrl+t' -ScriptBlock { Fuzzy-File-Get }
+Set-PSReadLineKeyHandler -Key 'alt+d' -ScriptBlock { Fuzzy-Directory-Get } 
 
-
-$Env:EDITOR="vim.bat"
-$Env:_PSFZF_FZF_DEFAULT_OPTS="--height 40% --reverse"
-$Env:FZF_DEFAULT_OPTS = '--bind ctrl-a:select-all,ctrl-d:deselect-all'
-$Env:FZF_DEFAULT_COMMAND = 'ag --hidden --ignore .git -g .'
+# FIXME alt+c is very slow as it searches through appdata, fleet cleaner etc. Think of a way to solve this.
 
