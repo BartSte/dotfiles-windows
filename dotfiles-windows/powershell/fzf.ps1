67db9843aa781a3c4dcbd4824d108e2792fc9bef
barts <b.steensma@fleetcleaner.com>
2022-05-02 10:14:56 +0200

    improved fzf commands

--- dotfiles-windows/powershell/fzf.ps1
+++ dotfiles-windows/powershell/fzf.ps1
@@ -1,8 +1,24 @@
 $Env:EDITOR="gvim.bat"
 $Env:_PSFZF_FZF_DEFAULT_OPTS="--height 40% --reverse"
 $Env:FZF_DEFAULT_OPTS = '--bind ctrl-a:select-all,ctrl-d:deselect-all'
-$Env:FZF_DEFAULT_COMMAND = 'ag --hidden --ignore .git --ignore AppData --ignore "Reporting Data" --ignore "Fleet Cleaner" -g .'
 
+$Env:FZF_DEFAULT_COMMAND = -join @('ag --hidden '
+    '--ignore .git '
+    '--ignore *.vscode* '
+    '--ignore *.dot* '
+    '--ignore *.venv* '
+    '--ignore AppData '
+    '--ignore "Fleet Cleaner"'
+    '-g .')
+
+$Env:FZF_ALT_C_COMMAND = -join @('Get-ChildItem . -Recurse -Directory |'
+    'Where {'
+        '$_.FullName -notlike "*\.vscode\*" -and '
+        '$_.FullName -notlike "*.venv*" -and '
+        '$_.FullName -notlike "Fleet Cleaner" -and '
+        '$_.FullName -notlike "*.git*" -and '
+        '$_.FullName -notlike "*.dot*" '
+        '}')
 
 function Fuzzy-Directory-Get() {
     $file = Get-ChildItem . -Recurse -Attributes Directory | Invoke-Fzf 
@@ -15,6 +31,3 @@ Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' `
 Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
 Set-PSReadLineKeyHandler -Key 'alt+o' -ScriptBlock { Invoke-FuzzyEdit }
 Set-PSReadLineKeyHandler -Key 'alt+d' -ScriptBlock { Fuzzy-Directory-Get } 
-
-# FIXME alt+c is very slow as it searches through appdata, fleet cleaner etc. Think of a way to solve this.
-
