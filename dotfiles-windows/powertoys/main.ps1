b55e4f837530e6534a5b12966eda0c0b8ea798b2
barts <b.steensma@fleetcleaner.com>
2022-06-16 16:40:59 +0200

    add default.json keyboard manager powertoys

--- dotfiles-windows/powertoys/main.ps1
+++ dotfiles-windows/powertoys/main.ps1
@@ -1,8 +1,16 @@
 function Hard-Link-Keyboard-Settings {
+    $my_default = Join-Path -Path $PSScriptRoot -ChildPath "keyboard_default.json"
     $my_settings = Join-Path -Path $PSScriptRoot -ChildPath "keyboard_settings.json"
+
+    $power_toys_default = Join-Path -Path $env:LOCALAPPDATA -ChildPath "Microsoft" | Join-Path -ChildPath "PowerToys" | Join-Path -ChildPath "Keyboard Manager" | Join-Path -ChildPath "default.json";
     $power_toys_settings = Join-Path -Path $env:LOCALAPPDATA -ChildPath "Microsoft" | Join-Path -ChildPath "PowerToys" | Join-Path -ChildPath "Keyboard Manager" | Join-Path -ChildPath "settings.json";
+    
+    Remove-Item $power_toys_default -ErrorAction SilentlyContinue
     Remove-Item $power_toys_settings -ErrorAction SilentlyContinue
+
+    New-Item -ItemType HardLink -Path $power_toys_default -Target $my_default
     New-Item -ItemType HardLink -Path $power_toys_settings -Target $my_settings
+
     Write-Host "Make hardlink to keyboard Manager of PowerToys"
 }
 
