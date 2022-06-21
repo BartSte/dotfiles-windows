8e77cb9221a9c56b7af5dd9e6c5517559cdd563b
barts <b.steensma@fleetcleaner.com>
2022-06-21 16:25:53 +0200

    added aliases and fixed editor

--- dotfiles-windows/powershell/aliases.ps1
+++ dotfiles-windows/powershell/aliases.ps1
@@ -17,8 +17,8 @@ ${function:win} = { git.exe --git-dir="$HOME\.dotfiles-windows\" --work-tree=$HO
 
 ${function:ex} = {explorer.exe .}
 ${function:act} = {.venv/Scripts/Activate.ps1}
-${function:gvims} = {gvim.bat -S @args -c 'source ~/.vimrc|source ~/.gvimrc'}
-${function:vims} = {vim.bat -S @args -c 'source ~/.vimrc'}
+${function:vim} = {C:\tools\vim\vim82\gvim.exe @args}
+${function:gvim} = {C:\tools\vim\vim82\gvim.exe @args}
 ${function:test} = {python -m unittest discover -s @args}
 
 ${function:rm} = {Remove-ItemSafely @args}
--- dotfiles-windows/powershell/fzf.ps1
+++ dotfiles-windows/powershell/fzf.ps1
@@ -1,4 +1,4 @@
-$Env:EDITOR = "gvim.bat"
+$Env:EDITOR = "C:/tools/vim/vim82/gvim.exe"
 $Env:FZF_DEFAULT_COMMAND = "fd --hidden --no-ignore-vcs --max-depth 4 --ignore-file $HOME/.ignore -t f '' ."
 $Env:_PSFZF_FZF_DEFAULT_OPTS = "--height 80% --reverse"
 $Env:FZF_DEFAULT_OPTS = '--bind ctrl-a:select-all,ctrl-d:deselect-all'
