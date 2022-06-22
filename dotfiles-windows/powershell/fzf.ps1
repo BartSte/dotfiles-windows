7c0365295a8bff5f536d1c017c5459986c3c0ad1
barts <b.steensma@fleetcleaner.com>
2022-06-22 08:32:23 +0200

    Added fzf preview

--- dotfiles-windows/powershell/fzf.ps1
+++ dotfiles-windows/powershell/fzf.ps1
@@ -1,7 +1,10 @@
 $Env:EDITOR = "C:/tools/vim/vim82/vim.exe"
 $Env:FZF_DEFAULT_COMMAND = "fd --hidden --no-ignore-vcs --max-depth 4 --ignore-file $HOME/.ignore -t f '' ."
-$Env:_PSFZF_FZF_DEFAULT_OPTS = "--height 80% --reverse"
 $Env:FZF_DEFAULT_OPTS = '--bind ctrl-a:select-all,ctrl-d:deselect-all'
+$Env:_PSFZF_FZF_DEFAULT_OPTS = "--height 80% --reverse --border"
+
+$Env:FZF_CTRL_T_OPTS='--preview "bat --style=numbers --color=always --line-range :500 {}"'
+
 $Env:FZF_ALT_C_COMMAND = "fd --hidden --no-ignore-vcs --max-depth 4 --ignore-file $HOME/.ignore -t d '' ."
 
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' `
--- dotfiles-windows/powershell/profile.ps1
+++ dotfiles-windows/powershell/profile.ps1
@@ -1,10 +1,5 @@
 # Profile for the Microsoft.Powershell Shell, only. (Not Visual Studio or other PoSh instances)
 # ===========
-
-Import-Module -Name Terminal-Icons
-
-~/dotfiles-windows/powershell/helpers/set_font.ps1 'JetBrainsMono NF' 16
-
 Push-Location (Split-Path -parent $profile) 
 
 "functions", "aliases", "fzf" | 
