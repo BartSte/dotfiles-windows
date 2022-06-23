156fc2210263d5cd7c8b3bac5c88ad56b4d84628
barts <b.steensma@fleetcleaner.com>
2022-06-23 15:38:49 +0200

    update fzf

--- dotfiles-windows/powershell/fzf.ps1
+++ dotfiles-windows/powershell/fzf.ps1
@@ -1,11 +1,12 @@
 $Env:EDITOR = "C:/tools/vim/vim82/vim.exe"
-$Env:FZF_DEFAULT_COMMAND = "fd --hidden --no-ignore-vcs --max-depth 4 --ignore-file $HOME/.ignore -t f '' ."
+$Env:FZF_DEFAULT_COMMAND = "fd --hidden --no-ignore-vcs --max-depth 4 --ignore-file $HOME/.ignore -t d -t f'' ."
 $Env:FZF_DEFAULT_OPTS = '--bind ctrl-a:select-all,ctrl-d:deselect-all'
 $Env:_PSFZF_FZF_DEFAULT_OPTS = "--height 80% --reverse --border"
 
 $Env:FZF_CTRL_T_OPTS='--preview "bat --style=numbers --color=always --line-range :500 {}"'
 
 $Env:FZF_ALT_C_COMMAND = "fd --hidden --no-ignore-vcs --max-depth 4 --ignore-file $HOME/.ignore -t d '' ."
+$Env:FZF_ALT_C_OPTS = '--preview "tree /A /F {} | bat --style=numbers --color=always --line-range 3:100"'
 
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' `
                 -PSReadlineChordReverseHistory 'Ctrl+r'
