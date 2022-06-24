30dc4902d769c0102c1b9e57095721271b60c777
barts <b.steensma@fleetcleaner.com>
2022-06-24 08:57:53 +0200

    Improve fzf

--- dotfiles-windows/powershell/profile.ps1
+++ dotfiles-windows/powershell/profile.ps1
@@ -1,5 +1,7 @@
 # Profile for the Microsoft.Powershell Shell, only. (Not Visual Studio or other PoSh instances)
 # ===========
+Import-Module Terminal-Icons
+
 Push-Location (Split-Path -parent $profile) 
 
 "functions", "aliases", "fzf" | 
