555a358759db33c48d558384181a815d06a0c661
barts <b.steensma@fleetcleaner.com>
2022-06-27 20:47:05 +0200

    update general

--- dotfiles-windows/powershell/fzf.ps1
+++ dotfiles-windows/powershell/fzf.ps1
@@ -1,9 +1,10 @@
 $Env:SHELL = "powershell.exe"
 $Env:EDITOR = "C:/tools/vim/vim82/vim.exe"
-$Env:FZF_DEFAULT_COMMAND = "fd --hidden --no-ignore-vcs --max-depth 4 --ignore-file $HOME/.ignore -t d -t f'' ."
+# $Env:FZF_DEFAULT_COMMAND = "fd --hidden --no-ignore-vcs --max-depth 4 --ignore-file $HOME/.ignore -t d -t f '' ."
 $Env:FZF_DEFAULT_OPTS = '--bind ctrl-a:select-all,ctrl-d:deselect-all'
 $Env:_PSFZF_FZF_DEFAULT_OPTS = "--height 100% --reverse --border"
 
+$Env:FZF_CTRL_T_COMMAND="fd --hidden --no-ignore-vcs --max-depth 4 --ignore-file $HOME/.ignore -t d -t f '' ."
 $Env:FZF_CTRL_T_OPTS='--preview "bat --theme=gruvbox-dark --style=numbers --color=always --line-range :500 {}"'
 
 $Env:FZF_ALT_C_COMMAND = "fd --hidden --no-ignore-vcs --max-depth 4 --ignore-file $HOME/.ignore -t d '' ."
