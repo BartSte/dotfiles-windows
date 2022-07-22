fe69ccda279aad4ef93b1a0e599997eeadd652d8
Bart Steensma <b.steensma@fleetcleaner.com>
2022-07-22 19:08:13 +0200

    update bindings

--- dotfiles-windows/powershell/bindings.ps1
+++ dotfiles-windows/powershell/bindings.ps1
@@ -5,4 +5,5 @@ Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
 Set-PSReadLineKeyHandler -Key 'ctrl-f' -Function ViCommandMode
 Set-PSReadLineKeyHandler -Key 'ctrl-b' -ScriptBlock {[Microsoft.PowerShell.PSConsoleReadLine]::Insert($(git branch --all|Invoke-Fzf).substring(2))}
 Set-PSReadLineKeyHandler -Key 'alt-l' -Function ClearScreen
+Set-PSReadLineKeyHandler -Key 'alt-d' -Function DeleteWord
 
