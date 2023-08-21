97c06f0ae87c68c670f8ec461b93495a9bfbfd56
bartsteensma <b.steensma@fleetcleaner.com>
2023-08-21 22:18:36 +0200

    ""

--- dotfiles-windows/vim/helpers.ps1
+++ dotfiles-windows/vim/helpers.ps1
@@ -3,29 +3,39 @@ function symbolic-link-configuration() {
 
     $target = "${HOME}\dotfiles\nvim\init.lua"
     $path = "${env:LOCALAPPDATA}\nvim\init.lua" 
-    _symlink $path $target
+    New-Symlink $path $target
 
     $target = "${HOME}\dotfiles\nvim\lua"
     $path = "${env:LOCALAPPDATA}\nvim\lua"
-    _symlink $path $target
+    New-Symlink $path $target
 
     $target = "${HOME}\dotfiles\nvim\plugin"
     $path = "${env:LOCALAPPDATA}\nvim\plugin"
-    _symlink $path $target
+    New-Symlink $path $target
 
     $target = "${HOME}\dotfiles\nvim\vim"
     $path = "${env:LOCALAPPDATA}\nvim\vim"
-    _symlink $path $target
+    New-Symlink $path $target
 
     $target = "${HOME}\dotfiles\nvim\after"
     $path = "${env:LOCALAPPDATA}\nvim\after"
-    _symlink $path $target
+    New-Symlink $path $target
+
+    $target = "${HOME}\dotfiles\nvim\lazy-lock.json"
+    $path = "${env:LOCALAPPDATA}\nvim\lazy-lock.json"
+    New-Hardlink $path $target
 }
 
-function _symlink($path, $target) {
+function New-Symlink($path, $target) {
     write-host $path
     Remove-Item $path -ErrorAction SilentlyContinue -Recurse
     New-Item -ItemType SymbolicLink -Path $path -Target $target
+}
+
+function New-Hardlink($path, $target) {
+    write-host $path
+    Remove-Item $path -ErrorAction SilentlyContinue -Recurse
+    New-Item -ItemType Hardlink -Path $path -Target $target
 }
 
 function enable_python() {
