58e1233f24b537df53e80300e2e8e9ec33116406
bartsteensma <b.steensma@fleetcleaner.com>
2024-03-11 18:27:21 +0100

    ""

--- dotfiles-windows/powershell/profile.ps1
+++ dotfiles-windows/powershell/profile.ps1
@@ -1,9 +1,3 @@
 Invoke-Expression (oh-my-posh --init --shell pwsh --config $HOME\dotfiles-windows\posh\gruvbox.omp.json)
-
-Push-Location (Split-Path -parent $profile) 
-
-"env", "vi_mode", "functions", "aliases", "bindings" | 
-Where-Object { Test-Path "$_.ps1" } | 
-ForEach-Object -process { Invoke-Expression ". .\$_.ps1" }
-
-Pop-Location
+$dir="$HOME\dotfiles-windows\powershell"
+"env", "vi_mode", "functions", "aliases", "bindings" | ForEach-Object { . "$dir\$_" }
