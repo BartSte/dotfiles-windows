93d4ef98d5767a4c91a03baaad075c2a0544bc47
bartsteensma <b.steensma@fleetcleaner.com>
2024-03-13 09:55:21 +0100

    Automatic update

--- dotfiles-windows/capslock/main.ps1
+++ dotfiles-windows/capslock/main.ps1
@@ -1,5 +1,27 @@
+function Download-CapslockIndicator($version)
+{
+    $source="https://github.com/jonaskohl/CapsLockIndicator/releases/download/$version/CLIv3-$version.exe"
+    $destination_dir="$Env:LOCALAPPDATA\capslockindicator"
+    $destination="$destination_dir\capslockindicator.exe"
+
+    Write-Host "Downloading CapsLockIndicator version: $version"
+    Write-Host "URL: $source"
+    Write-Host "Saving to: $destination"
+
+    if (-not (Test-Path $destination_dir))
+    {
+        New-Item -ItemType Directory -Path $destination_dir
+    } else
+    {
+        Remove-Item $destination -ErrorAction SilentlyContinue
+    }
+
+    Invoke-WebRequest -Uri $source -OutFile $destination
+}
+
+
 function New-Symlink-Exe {
-    $target = "$HOME\dotfiles-windows\capslock\CLIv3-3.16.1.2.exe"
+    $target = "$Env:LOCALAPPDATA\capslockindicator\capslockindicator.exe"
     $path = "$HOME\Desktop\Capslock-indicator"
 
     write-host "SymbolicLink from $target to $path"
@@ -16,5 +38,6 @@ function New-Symlink-Config {
     New-Item -ItemType SymbolicLink -Path $path -Target $target -Force
 }
 
+Download-CapslockIndicator "3.16.1.2"
 New-Symlink-Exe
 New-Symlink-Config
